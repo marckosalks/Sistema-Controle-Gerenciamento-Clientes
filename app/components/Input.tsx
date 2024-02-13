65f16978bd11c3e7f4bc7a61a7f069542ef2075d
Marcos Sales <m4rcos.sales@gmail.com>
2024-02-13 13:29:24 -0300

    implementando hook form

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,16 +1,23 @@
 import classNames from 'classnames';
 
+import { RegisterOptions } from 'react-hook-form';
+
 type InputProps = {
   type: "text" | "password" | "email" | "number" | "date"
   className?: string
+  register: any
+  name: string
 }
 
-export default function Button({ className, type, ...rest }: InputProps) {
+export default function Button({ className, type, name, register, ...rest }: InputProps) {
   const baseClasses = "rounded-md outline-none border  border-blue-500 p-1"
 
   const mergedClasses = classNames(baseClasses, className)
   
   return (
-    <input className={ mergedClasses}  type={type} {...rest} />
+    <input className={ mergedClasses}  
+    type={type}
+    {...register(name)}
+    {...rest} />
   )
 }
--- app/register/CreateUserFom.tsx
+++ app/register/CreateUserFom.tsx
@@ -1,31 +1,41 @@
+'use client'
+
+import * as yup from 'yup'
+
 import Button from "@/app/components/Button";
 import Input from "@/app/components/Input";
-
 import Span from "@/app/components/Span";
+import { useForm } from 'react-hook-form';
 
+//Aqui vou resgatar as funcionalidades do useHookForm
 
 export default function CreateUserFom() {
+
+  const { register, handleSubmit } = useForm()
+
   return (
-    <form className='flex flex-col gap-3 items-center'>
+    <form onSubmit={handleSubmit((data) => console.log(data))}
+    className='flex flex-col gap-3 items-center'>
+      
       <div className='flex flex-col  w-[30%] gap-2'>
         <Span label="🧹Nome:"/>
-        <Input type="text" />
+        <Input name="nome" type="text" register={register} />
       </div>
 
-      <div className='flex flex-col  w-[30%] gap-2'>
+       <div className='flex flex-col  w-[30%] gap-2'>
         <Span label="🧹Email:"/>
-        <Input type="email" />
+        <Input name='email' type="email" register={register}/>
       </div>
 
       <div className='flex flex-col  w-[30%] gap-2'>
         <Span label="🧹Telefone:"/>
-        <Input type="text" />
+        <Input type="text" name='telefone' register={register} />
       </div>
 
       <div className='flex flex-col  w-[30%] gap-2'>
         <Span label="🧹Cep:"/>
-        <Input type="text" />
-      </div>
+        <Input type="text" name='cep' register={register}/>
+      </div> 
 
       <Button  type="submit" label="Enviar"/>
     </form>
